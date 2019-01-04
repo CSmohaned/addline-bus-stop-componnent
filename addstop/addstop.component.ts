0d812857459d22b486310991070bb00c17b85599
CSmohaned <36245878+CSmohaned@users.noreply.github.com>
2019-01-04 17:04:47 +0200

    Add files via upload
    Addstop

---
+++ addstop/addstop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addstop',
+  templateUrl: './addstop.component.html',
+  styleUrls: ['./addstop.component.scss']
+})
+export class AddstopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
